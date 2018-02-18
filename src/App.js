31e48b5df18b6470423ad6302ad2e76067f9f3e0
Thai Le <thai.viet.le@gmail.com>
2018-02-18 14:51:12 -0500

    fix query

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
@@ -9,7 +9,14 @@ const GET_SESSIONS = gql`
     sessions {
       _id
       date
-      activities
+      activities {
+        exerciseId
+        sets {
+          weight
+          reps
+          duration
+        }
+      }
     }
   }
 `;

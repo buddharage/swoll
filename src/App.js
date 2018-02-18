dd01b4b1a34749c720a41378c6c4e7047badfbb0
Thai Le <thai.viet.le@gmail.com>
2018-02-18 14:48:08 -0500

    add Apollo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+const GET_SESSIONS = gql`
+  query SessionsQuery {
+    sessions {
+      _id
+      date
+      activities
+    }
   }
+`;
+
+function App(props) {
+  console.log(props.data);
+  return <pre />;
 }
 
-export default App;
+export default graphql(GET_SESSIONS)(App);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
+
+// App specific imports
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  link: createHttpLink({ uri: 'https://swoll-api.herokuapp.com/graphql' }),
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
